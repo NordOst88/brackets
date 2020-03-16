6069f6878a1117cf96694c2cd40f98f8c226cfc7
NordOst88 <NordOst88@users.noreply.github.com>
2020-03-17 02:46:38 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+for (let i = 0; i < str.length; i++) {             
+    for (let j = 0; j < bracketsConfig.length; j++) { 
+      if (str.includes(bracketsConfig[j].join(''))) {            
+          str = str.replace(bracketsConfig[j].join(''), ''); 
+           i = -1;
+         continue;          
+      };
+    };
+  };
+  
+  return str.length == 0 ? true : false;
+
+};
